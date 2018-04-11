a05f8674f40379680b8508df53faf4440d91d643
Yuriy Prezlyata <yprezlyata@GW-GridDynamics.retn.net>
2018-04-11 16:55:25 +0300

    initial component rc-slidepanel

--- src/index.js
+++ src/index.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import cx from 'classnames';
+import 'index.css';
 
-class TestComponent extends React.Component {
+class Slidepanel extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
-        return (
-            <div>This is so test component!</div>
+        const {
+            isPanelOpen,
+            closePanel,
+            right,
+            left,
+            top,
+            bottom,
+            width,
+            height,
+            bgColor,
+            children
+        } = this.props;
+
+        const CN = cx(
+            'slidepanel',
+            {'right': right},
+            {'left': left},
+            {'top': top},
+            {'bottom': bottom}
         );
+
+        const PanelStyle = {
+            width: width,
+            height: height,
+            backgroundColor: bgColor
+        };
+
+        if(!isPanelOpen) { return null; }
+
+        return (
+            <div className={cx(CN)} style={PanelStyle}>
+                <div className='wrapp_close_btn'>
+                    <div
+                        className='btn_close_panel'
+                        onClick = { closePanel }
+                    />
+                </div>
+                {children}
+            </div>
+        )
     }
 }
 
-export default TestComponent;
+export default Slidepanel;
